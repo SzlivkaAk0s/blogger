1f6261a5d4a648ea6962cb0b6944f15c9564f05f
SzlivkaAk0s <szlivkaa@kkszki.hu>
2026-09-21 10:52:03 +0200

    new model created

---
+++ BlogAPI/Models/Blogpost.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogAPI.Models
+{
+    public class Blogpost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Content { get; set; }
+        public DateTime postTime { get; set; }
+        public DateTime updateTime { get; set; }
+        public int blogId { get; set; }
+    }
+}
