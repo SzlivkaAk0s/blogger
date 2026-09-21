230aeddcd1ad4b4491d6cc814274fdeea85e504d
SzlivkaAk0s <szlivkaa@kkszki.hu>
2026-09-21 11:00:53 +0200

    get method

--- BlogAPI/Controllers/BlogpostController.cs
+++ BlogAPI/Controllers/BlogpostController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlogAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
 
 namespace BlogAPI.Controllers
 {
@@ -7,5 +9,39 @@ namespace BlogAPI.Controllers
     [ApiController]
     public class BlogpostController : ControllerBase
     {
+        public string ConnectionString = "server=localhost;uid=root;password=;database=blog;";
+        [HttpGet]
+        public List<Blogpost> GetPosts()
+        {
+            List<Blogpost> posts = new List<Blogpost>();
+
+            var connection = new MySqlConnection(ConnectionString);
+
+            connection.Open();
+
+            string sql = "SELECT * FROM `blogpost`;";
+
+            var cmd = new MySqlCommand(sql, connection);
+
+            var data = cmd.ExecuteReader();
+
+            while (data.Read())
+            {
+                var post = new Blogpost()
+                {
+                    Id = data.GetInt32("id"),
+                    Title = data.GetString("title"),
+                    Content = data.GetString("content"),
+                    postTime = data.GetDateTime("postTime"),
+                    updateTime = data.GetDateTime("updateTime"),
+                    blogId = data.GetInt32("blogId"),
+                };
+                posts.Add(post);
+            }
+
+            connection.Close();
+
+            return posts;
+        }
     }
 }
